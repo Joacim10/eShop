aa4fd215b44081acf13a023c88227ecb04a8f359
Anton Eliasson <antoneliasson@MBPsomthorAnton.lan>
2020-09-15 20:07:45 +0200

    Quotes Slide

---
+++ src/components/Homepage/QuotesSlide.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="mt-5 container-fluid bgDarkBlue">
+   <div class="container mt-1">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                    <i class="fa-5x fas fa-quote-right theme"></i>
+                    </div>
+                    <div class=" col-lg-12 text-center mt-4">
+                        <img src="/image/Quotes/QuotesImage.png">
+                    </div>
+                    <div class=" col-lg-12 text-center mt-3">
+                        <i class=" starsActive fas fa-star"></i>
+                        <i class=" starsActive fas fa-star"></i>
+                        <i class="starsActive fas fa-star"></i>
+                        <i class="starsActive fas fa-star"></i>
+                        <i class=" theme far fa-star"></i>
+                    </div>
+                    <div class="col-lg-12 text-center mt-3">
+                        <span class="theme pb-1 text-uppercase">Johhny WessMullar</span> <br>
+                        <small class="text-white ">CEO, Biotech Inc.</small>
+                    </div>
+                    <div class="col-lg-10 offset-lg-1 text-center text-white pb-5 mt-2 text-wrap">
+                        <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.
+                        </p>
+                    </div>
+                </div>
+            </div>
+           </div> 
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.starsActive{
+  color: var(--theme);
+}
+</style>
