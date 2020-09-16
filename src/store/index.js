07235e2778990c63de2dbb3e2b53588803b6991b
Jonas Mets <jonas.mets@outlook.com>
2020-09-16 09:43:33 +0200

    Cart

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import Vuex from 'vuex'
 
 // importerar modulerna för produkter och shoppingcart
 import storeProducts from './storeModules/storeProducts';
-// import storeCart from './storeModules/storeCart';
+import storeCart from './storeModules/storeCart';
 // import storeUser from './storeModules/storeUser';
 // import storeOrders from './storeModules/storeOrders';
 
@@ -14,7 +14,8 @@ export default new Vuex.Store({
   // använder de importerade modulerna från storeProducts.js och storeCart.js
   // som innehåller state, mutations, actions, getters
   modules: {
-    storeProducts
+    storeProducts,
+    storeCart
     
   }
 })
