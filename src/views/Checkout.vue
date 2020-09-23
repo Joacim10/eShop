1dd08e50b2a4561b2e374779103f2c9950977db9
Jonas Mets <jonas.mets@outlook.com>
2020-09-23 11:57:50 +0200

    lagt till logik

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -8,33 +8,34 @@
           <div class="col">
             <p>
               Returning customer?
-              <a href="#" class="theme">Click here to login</a>
+              <!-- <a href="#" class="theme">Click here to login</a> -->
+              <router-link to="/account" class="theme" >Click here to login</router-link>
             </p>
             <p>
               Have a coupon?
               <a href="#" class="theme">Click here to enter your code</a>
             </p>
 
-            <CheckoutCupon />
+            <div class="mt-3 mb-3">
+              <CheckoutCupon />
+            </div>
+
             <CheckoutForm />
           </div>
 
           <!-- Col 2 -->
           <div class="col">
-
             <div class="card">
-
               <div class="card-header bgDarkBlue text-center theme f-24 py-1">
                 <span class="text-uppercase f24">Your order</span>
               </div>
 
-  <div class="card-body">
-              <CheckoutOrder />
-              <CheckoutPlaceorder />
-  </div>
+              <div class="card-body">
+                <CheckoutOrder />
+                <CheckoutPlaceorder />
+              </div>
             </div>
           </div>
-
         </div>
       </form>
     </div>
@@ -43,7 +44,9 @@
 
 <script>
 import CheckoutBreadcrumb from "@/components/Checkout/CheckoutBreadcrumb.vue";
-import CheckoutCupon from "@/components/Checkout/CheckoutCupon.vue";
+// import CheckoutCupon from "@/components/Checkout/CheckoutCupon.vue";
+import CheckoutCupon from "@/components/ReusableComponents/ApplyCouponComponent.vue";
+
 import CheckoutForm from "@/components/Checkout/CheckoutForm.vue";
 import CheckoutOrder from "@/components/Checkout/CheckoutOrder.vue";
 import CheckoutPlaceorder from "@/components/Checkout/CheckoutPlaceorder.vue";
