91bf1771dc50a1c80c6edef87abb2871e06c2868
Joacim Hammarström <joacimhammarstrom@gmail.com>
2020-10-02 13:24:32 +0200

    carousel, pagination

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="container">
-        <div class="grid-container">
+  <div class="container productsPage">
+    <div class="grid-container">
       <div class="menu text-left pr-md-3">
         <div class="f-16 theme cursorNormal">Categories</div>
         <div class="f-16 d-flex justify-content-between" :class="{ theme : activeCategory === 'Men' }"> <div class="cursorPointer" v-on:click="changeCategory('Men')"  >Men's</div> <div class="cursorNormal">{{ countCategory('Men').length }}</div> </div>
@@ -11,8 +11,6 @@
         <div class="f-16 d-flex justify-content-between" :class="{ theme : activeCategory === 'Shoes' }" > <div class="cursorPointer" v-on:click="changeCategory('Shoes')" >Shoes</div> <div class="cursorNormal">{{ countCategory('Shoes').length }}</div> </div>
         <div class="f-16 d-flex justify-content-between" :class="{ theme : activeCategory === 'Watches' }" > <div class="cursorPointer" v-on:click="changeCategory('Watches')" >Watches</div> <div class="cursorNormal">{{ countCategory('Watches').length }}</div> </div>
 
-<!--         <div class="f-16 theme head">Filter By Price</div> -->
-
         <div class="f-16 theme head cursorNormal">Filter By Color</div>
         <div v-if="countColor('#000000') != 0" class="f-16 d-flex justify-content-between align-items-center"><div class="d-flex align-items-center"><input class="black cursorPointer" type="checkbox" v-model="colors" value="#000000" /><div class="color cursorNormal">Black</div></div> <div class="cursorNormal">{{ countColor('#000000') }}</div> </div>
         <div v-if="countColor('#808080') != 0" class="f-16 d-flex justify-content-between align-items-center"><div class="d-flex align-items-center"><input class="grey cursorPointer" type="checkbox" v-model="colors" value="#808080" /><div class="color cursorNormal">Grey</div></div> <div class="cursorNormal">{{ countColor('#808080') }}</div> </div>
@@ -21,46 +19,17 @@
         <div v-if="countColor('#F00B07') != 0" class="f-16 d-flex justify-content-between align-items-center"><div class="d-flex align-items-center"><input class="red cursorPointer" type="checkbox" v-model="colors" value="#F00B07" /><div class="color cursorNormal">Red</div></div> <div class="cursorNormal">{{ countColor('#F00B07') }}</div> </div>
         <div v-if="countColor('#F385E9') != 0" class="f-16 d-flex justify-content-between align-items-center"><div class="d-flex align-items-center"><input class="pink cursorPointer" type="checkbox" v-model="colors" value="#F385E9" /><div class="color cursorNormal">Pink</div></div> <div class="cursorNormal">{{ countColor('#F385E9') }}</div> </div>
         <div v-if="countColor('#ffffff') != 0" class="f-16 d-flex justify-content-between align-items-center"><div class="d-flex align-items-center"><input class="whiteBox cursorPointer" type="checkbox" v-model="colors" value="#ffffff" /><div class="color cursorNormal">White</div></div> <div class="cursorNormal">{{ countColor('#ffffff') }}</div> </div>
-
-<!--         <div class="f-16 theme head">Filter By Size</div>
-
-        <div class="f-16 d-flex justify-content-between"> <div class="">XS</div> <div>12</div> </div>
-        <div class="f-16 d-flex justify-content-between"> <div class="">S</div> <div>12</div> </div>
-        <div class="f-16 d-flex justify-content-between"> <div class="">M</div> <div>12</div> </div>
-        <div class="f-16 d-flex justify-content-between"> <div class="">L</div> <div>12</div> </div>
-        <div class="f-16 d-flex justify-content-between"> <div class="">XL</div> <div>12</div> </div>
-        <div class="f-16 d-flex justify-content-between"> <div class="">XXL</div> <div>12</div> </div> -->
-
-<!--         <div class="f-16 theme head">Filter By Brand</div>
-        <div class="row brands">
-          <div class="col-6">
-            <img src="/Image/Brand/Bexim.png" alt="">
-          </div>
-          <div class="col-6">
-            <img src="/Image/Brand/Lighton.png" alt="">
-          </div>
-          <div class="col-6"> 
-            <img src="/Image/Brand/Dismis.png" alt="">
-          </div>
-          <div class="col-6">
-            <img src="/Image/Brand/Rosimo.png" alt="">
-          </div>
-          <div class="col-6">
-            <img src="/Image/Brand/Dallas.png" alt="">        
-          </div>
-        </div> -->
-
-  <!--       <div class="f-16 theme head">Product Status</div>
-        <div class="f-16 theme head">Top Rated Products</div> -->
         <div class="d-flex justify-content-start mt-4">
           <button @click="clearFilter" class="theme btn btnTheme white">Clear <span class="d-none d-sm-inline-block">Filter</span></button>
         </div>
       </div>
       <div class="products">
-        <ProductList v-if="computedProducts" :products="computedProducts"/>
+        <ProductList v-if="productsOnPage" :products="productsOnPage"/>
       </div>
-      <div class="pagination d-flex justify-content-center">
-        <div><!-- Pagination --></div> 
+      <div v-if="numberOfPages !== 1" class="pagination d-flex justify-content-center">
+        <div v-for="(x, index) in numberOfPages" :key="index">
+          <p class="d-inline-block font-weight-bold px-2 cursorPointer" :class="{ 'theme' : page === x }" v-on:click="changePage(x)">{{x}}</p> 
+        </div> 
       </div>
     </div>
   </div>
@@ -80,16 +49,22 @@ export default {
     return {
       activeCategory: '',
       colors: [],
-      activeFilter: ''
+      activeFilter: '',
+      page: 1,
     }
   },
   methods: {
+    changePage: function (page) {
+      this.page = page
+    },
     changeCategory: function (category) {
       if (this.activeCategory != category) {
         this.colors = []
+        this.page = 1
         this.$router.push({path: '/products', query: { category: category } })
       } else {
         this.colors = []
+        this.page = 1
         this.$router.push({path: '/products'})
       }
     },
@@ -122,12 +97,23 @@ export default {
         this.$router.push({path: '/products'})
         this.activeCategory = ''
         this.activeFilter = ''
-        //console.log('rensar')
+        this.page = 1
       }
     }
   },
   computed:{
     ...mapGetters(['products']),
+    productsOnPage: function () {
+      let startAtProduct = ((this.page - 1) * 9)
+      let endAtProduct = startAtProduct + 9
+      let products = this.computedProducts.slice(startAtProduct, endAtProduct).map(i => {
+        return i
+      })
+      return products
+    },
+    numberOfPages: function () {
+      return Math.ceil(this.computedProducts.length / 9)
+    },
     computedProducts: function () {
       let newArrayOfProducts = this.products.data
 
@@ -143,7 +129,6 @@ export default {
         newArrayOfProducts = newArrayOfProducts.filter((item) => {
           return (item.badgetype === this.activeFilter) 
         }) 
-        //console.log('newArrayOfProducts 1', newArrayOfProducts)
       }
 
       // Filtrerar på varje color och slår ihop det till en array
@@ -175,28 +160,27 @@ export default {
   },
   updated() {
     this.activeCategory = this.$route.query.category || ''
-    //console.log('this.$route.query.filter', this.$route.query.filter)
-    //console.log('updated', this.$route.query.filter)
     this.activeFilter = this.$route.query.filter || ''
   },
   watch: {
     category: function(newVal) {
-      //console.log('newVal category !!!', newVal)
-
       this.activeCategory = newVal || ''
     },
     filter: function(newVal) {
-      //console.log('newVal filter !!!!!!!', newVal)
       this.activeFilter = newVal || ''
+      this.page = 1
     }
   }
 };
 </script>
 
 <style scoped>
-  .color {
-    position: relative;
-  }
+.productsPage {
+  min-height: 1100px 
+}
+.color {
+  position: relative;
+}
 
 input[type='checkbox']{
   width: 20px !important;
@@ -236,7 +220,7 @@ input.yellow[type='checkbox']:checked:after {content: '\2713';color: #808080;pos
     width: 225px;
   }
   .menu { grid-area: menu; }
-  .products { grid-area: products; }
+  .products { grid-area: products; min-height: 1000px; }
   .pagination { grid-area: pagination; }
   .grid-container {
     width: 100%;
