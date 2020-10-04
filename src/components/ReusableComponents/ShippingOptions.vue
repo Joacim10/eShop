0ad40a1d0a73d57aa4e5f2208ecbc3beea573f49
Jonas Mets <jonas.mets@outlook.com>
2020-10-04 11:35:07 +0200

    logik för cost shipp opt.

--- src/components/ReusableComponents/ShippingOptions.vue
+++ src/components/ReusableComponents/ShippingOptions.vue
@@ -36,12 +36,14 @@
               </span>
               <br />
             </div>
-            <div class="d-flex justify-content-end mt-1">
+            <!-- <div class="d-flex justify-content-end mt-1">
               <span>Shipping to AL</span>
-            </div>
-            <div class="d-flex justify-content-end mt-1">
+            </div> -->
+            <!-- <div class="d-flex justify-content-end mt-1">
               <span class>Change adress</span>
-            </div>
+            </div> -->
+            <!-- jm test -->
+            <!-- {{ shippingCost }} -->
           </div>
         </div>
 </template>
@@ -52,21 +54,41 @@ export default {
 data() {
     return {
       shippingType:'',
+      shippingCost:0,
     
     };
   },
   methods: {
-       ...mapActions(['updateshippingOption']),
+       ...mapActions(['updateshippingOption','updateShippingCost']),
+
+      calcshippingCost: function (shippingType) {
+      // console.log(code);
+      switch (shippingType) {
+        case "Flate rate":
+          return 20;
+        case "Free shipping":
+          return 0;
+        case "Local Pickup":
+          return 20;
+        default:
+          return 0;
+      }
+    },
+
   },
  
   created (){
         //this.updateshippingOption(this.shippingType);
         this.shippingType = this.getShippingData;
-       console.log(this.shippingType)
+      //  console.log(this.shippingType)
   },
   updated (){
       this.updateshippingOption(this.shippingType)
-       console.log(this.shippingType)
+      //  console.log(this.shippingType)
+      //  jm 20-10-04
+      this.shippingCost = this.calcshippingCost(this.shippingType);
+      this.updateShippingCost(this.shippingCost)
+
   },
   computed: {
       ...mapGetters(['getShippingData','setShoppingOption'])
