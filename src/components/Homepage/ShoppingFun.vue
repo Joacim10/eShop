24b4b9eb9d71aba591c4b26d39bd3dd2c594293a
Tina Källklint <t.kallklint@gmail.com>
2020-10-05 10:21:45 +0200

    fun

--- src/components/Homepage/ShoppingFun.vue
+++ src/components/Homepage/ShoppingFun.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="carousel slide pl-0 pr-0" data-ride="carousel">
-        <div class="productBg carousel-inner mb-first">
+        <div class="productBg carousel-inner mb-first"> 
             <div class="mtHome carousel-item active">
                 <div class="container">
                     <h1>{{ $t("header.shoping") }}</h1>
@@ -8,15 +8,24 @@
                     <p>Vivamus dignissim a ex eu ornare. Ut fermentum orci <br> vel diam ultricies faucibus. Quisque quis erat sed</p>
                     <router-link to="/products"><button class="btn btnWhite pl-4 pr-4 p-2">{{ $t("header.button") }}</button></router-link>
                 </div>
-                <img src="/Image/ShoppingFun/homeBg.png" class="d-block imgCarousel img-none">
+             
+                <img src="/Image/ShoppingFun/header-img.png" class="d-block imgCarousel img-none">
+                <!-- <img src="/Image/ShoppingFun/model_3.png " class="d-block imgCarousel img-none"> -->
+     
             </div>
         </div>
+
     </div>
+
+
+
 </template>
 
 <style scoped>
+
 .mtHome{
     margin-top: 5rem;
+    height: 400px;
 }
 
 .imgCarousel{
