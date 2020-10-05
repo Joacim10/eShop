90639d842ff856628901df61cff1e02b2e2dde9d
Jonas Mets <jonas.mets@outlook.com>
2020-10-05 11:56:36 +0200

    checkouttooltip

--- src/components/Checkout/CheckoutOrder.vue
+++ src/components/Checkout/CheckoutOrder.vue
@@ -36,7 +36,7 @@
     <div class="mt-3 mb-2">
       <div class="d-flex justify-content-between align-items-center">
         <p class="font-weight-bold text-uppercase theme mb-0">TOTAL</p>
-        <p class="theme mb-0">${{ shoppingCartTotal + getShippingCost}}</p>
+        <p class="theme mb-0">${{ shoppingCartTotal + getShippingCost }}</p>
       </div>
       <!-- <div v-if="getShippingCost > 0" class="mt-0 mb-0">
         <p class="text-right mt-0 mb-0">+ ${{ getShippingCost }} Shipping</p>
@@ -50,15 +50,58 @@
       <div class="border-bottom mt-3 pb-3">
         <div>
           <div class="d-flex">
-            <span>
-              <input
-                type="radio"
-                aria-label="Radio button for following text input"
-                value="Direct bank transfer"
-                v-model="paymentType"
-              />
-            </span>
-            <span class="ml-2">Direct bank transfer</span>
+            <CustomToolTip
+              :customInputData="[
+                {
+                  data: false,
+                  text:
+                    'Make your payment directly into our bank account. Please use your Order ID as the payment reference. Your order won’t be shipped until the funds have cleared in our account.',
+                },
+              ]"
+              v-bind:show="true"
+            >
+              <span>
+                <input
+                  type="radio"
+                  aria-label="Radio button for following text input"
+                  id="Directbanktransfer"
+                  value="Direct bank transfer"
+                  v-model="paymentType"
+                />
+              </span>
+              <!-- <span class="ml-2">Direct bank transfer</span> -->
+              <label for="Directbanktransfer" class="ml-2"
+                >Direct bank transfer</label
+              >
+            </CustomToolTip>
+            <br />
+          </div>
+        </div>
+
+        <div>
+          <div class="d-flex">
+            <CustomToolTip
+              :customInputData="[
+                {
+                  data: false,
+                  text:
+                    'Make your payment with check. Please use your Order ID as the payment reference.',
+                },
+              ]"
+              v-bind:show="true"
+            >
+              <span>
+                <input
+                  type="radio"
+                  aria-label="Radio button for following text input"
+                  id="CheckPayment"
+                  value="Check Payment"
+                  v-model="paymentType"
+                />
+              </span>
+              <!-- <span class="ml-2">Check Payment</span> -->
+              <label for="CheckPayment" class="ml-2">Check Payment</label>
+            </CustomToolTip>
 
             <br />
           </div>
@@ -66,31 +109,28 @@
 
         <div>
           <div class="d-flex">
-            <span>
-              <input
-                type="radio"
-                aria-label="Radio button for following text input"
-                value="Check Payment"
-                v-model="paymentType"
-              />
-            </span>
-            <span class="ml-2">Check Payment</span>
-
-            <br />
-          </div>
-        </div>
-
-        <div>
-          <div class="d-flex">
-            <span>
-              <input
-                type="radio"
-                aria-label="Radio button for following text input"
-                value="Cash on delivery"
-                v-model="paymentType"
-              />
-            </span>
-            <span class="ml-2">Cash on delivery</span>
+            <CustomToolTip
+              :customInputData="[
+                {
+                  data: false,
+                  text:
+                    'Make your payment with cash on delivery. Please use your Order ID as the payment reference.',
+                },
+              ]"
+              v-bind:show="true"
+            >
+              <span>
+                <input
+                  type="radio"
+                  aria-label="Radio button for following text input"
+                  id="Cashondelivery"
+                  value="Cash on delivery"
+                  v-model="paymentType"
+                />
+              </span>
+              <!-- <span class="ml-2">Cash on delivery</span> -->
+              <label for="Cashondelivery" class="ml-2">Cash on delivery</label>
+            </CustomToolTip>
 
             <br />
           </div>
@@ -98,21 +138,35 @@
 
         <div>
           <div class="d-flex align-items-end">
-            <span>
-              <input
-                type="radio"
-                aria-label="Radio button for following text input"
-                value="Paypal"
-                v-model="paymentType"
-              />
-            </span>
-            <span class="ml-2">Paypal</span>
-            <span class="theme ml-2">
-              <img src="/Image/checkout/PayPalLogo.png" />
-            </span>
-            <span class="ml-2 small">
-              <a href="#" class="grey">What is Paypal?</a>
-            </span>
+            <CustomToolTip
+              :customInputData="[
+                {
+                  data: false,
+                  text:
+                    'Make your payment with paypal. Please use your Order ID as the payment reference.',
+                },
+              ]"
+              v-bind:show="true"
+            >
+              <span>
+                <input
+                  type="radio"
+                  aria-label="Radio button for following text input"
+                  id="Paypal1"
+                  value="Paypal"
+                  v-model="paymentType"
+                />
+              </span>
+              <!-- <span class="ml-2">Paypal</span> -->
+              <label for="Paypal1" class="ml-2">Paypal</label>
+
+              <span class="theme ml-2">
+                <img src="/Image/checkout/PayPalLogo.png" />
+              </span>
+              <span class="ml-2 small">
+                <a href="#" class="grey">What is Paypal?</a>
+              </span>
+            </CustomToolTip>
 
             <br />
           </div>
@@ -149,8 +203,7 @@
       </div>
 
       <!-- knapp går bara att trycka på om shipping data är valid och man har godkänt vilkoren -->
-
-      <button
+      <!-- <button
         type="submit"
         class="btn btnTheme white col-lg-auto text-uppercase text-bold"
         v-bind:disabled="
@@ -165,8 +218,49 @@
         "
       >
         Place order
-      </button>
+      </button> -->
       <!-- <p>{{ shippingDataValid }} {{ acceptTerms }} {{ isUserLoggedIn }}</p> -->
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <!-- <div class="col-12"> -->
+        <!-- test -->
+        <CustomToolTip
+        class="col-12"
+          :customInputData="[
+            { data: shippingDataValid, text: 'Billing details not complete.' },
+            { data: acceptTerms, text: 'You must accept Terms.' },
+            { data: isUserLoggedIn, text: 'You must Log In.' },
+          ]"
+          v-bind:show="
+            !shippingDataValid ||
+            !acceptTerms ||
+            !isUserLoggedIn ||
+            !shoppingCart.length > 0
+          "
+        >
+          <button
+            type="submit"
+            class="btn btnTheme white text-uppercase text-bold col-12"
+            v-bind:disabled="
+              !shippingDataValid ||
+              !acceptTerms ||
+              !isUserLoggedIn ||
+              !shoppingCart.length > 0
+            "
+            v-on:click.prevent="
+              submitOrder();
+              scrollToTop();
+            "
+          >
+            Place order
+          </button>
+        </CustomToolTip>
+
+        <!-- test end -->
+        <!-- </div> -->
+      </div>
     </div>
   </div>
 </template>
@@ -174,12 +268,15 @@
 
 <script>
 import ShippingOptions from "../ReusableComponents/ShippingOptions";
+import CustomToolTip from "../ReusableComponents/CustomToolTip";
+
 import { mapGetters, mapActions } from "vuex";
 
 export default {
   name: "CheckoutOrder",
   components: {
     ShippingOptions,
+    CustomToolTip,
   },
   data() {
     return {
