e912600541af588ab2381b954a6b32a0535de709
Tina Källklint <t.kallklint@gmail.com>
2020-10-06 12:34:10 +0200

    owlCheck

--- src/components/Homepage/Quotes/QuotesSlide_bak.vue
+++ src/components/Homepage/Quotes/QuotesSlide_bak.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="mt-5 container-fluid bgDarkBlue">
    <div class="container mt-1">
+
                 <div class="row">
+                  
                     <div class="col-lg-12 text-center">
                     <i class="fa-5x fas fa-quote-right theme"></i>
                     </div>
+
                     <div class=" col-lg-12 text-center mt-4">
                         <img src="/image/Quotes/QuotesImage.png">
                     </div>
+
                     <div class=" col-lg-12 text-center mt-3">
                         <i class=" starsActive fas fa-star"></i>
                         <i class=" starsActive fas fa-star"></i>
@@ -24,19 +28,28 @@
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.
                         </p>
                     </div>
+                
                 </div>
+
+
+
             </div>
            </div> 
 </template>
 
 <script>
+
 export default {
 
-}
+components:{
+  
+  }
+};
+
 </script>
 
 <style scoped>
-.starsActive{
-  color: var(--theme);
-}
+  .starsActive{
+    color: var(--theme);
+  }
 </style>
